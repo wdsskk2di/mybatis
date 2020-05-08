fbd6a33fc4a1ff8b718b8717aafdde153753845a
123 <123@123>
2020-05-08 20:09:40 +0900

    DB연동. 데이터 선택으로 하나만 가져오기

--- src/main/java/com/care/dao/TestDAO.java
+++ src/main/java/com/care/dao/TestDAO.java
@@ -16,7 +16,7 @@ public class TestDAO {
 	private SqlSession sqlSession;
 	
 	public List<TestDTO> test(){	//기본경로 + ".mapper에서 만든, 실행할 쿼리문의 아이디"
-		return sqlSession.selectList(namespace+".listAll");	//여러 데이터 받아오기 위한 selectList.. 하나는 selectOne
+		return sqlSession.selectList(namespace+".listAll");	//여러 데이터 받아오기 위한 selectList.. 데이터 하나는 selectOne 으로 받아온다
 	}
 	
 	public TestDTO list(String num) {//기본경로 + ".mapper에서 만든, 실행할 쿼리문의 아이디", 입력받아서 넘겨줄값..(받는쪽은num = #{num}으로 받음)
